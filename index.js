74f72444b2a26eef83aa8b913b22fa3d1a644d80
jamilagastar <jamilagastar@gmail.com>
2022-04-30 17:24:45 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, passedInName) {
+    return drivers.filter((driverName) => driverName.toUpperCase() === passedInName.toUpperCase())
+}
+
+console.log(findMatching(drivers, "Ben"));
+
+function fuzzyMatch(drivers, passedInFirstTwoLetters) {
+    return drivers.filter((driverName) => driverName.substring(0,2) === passedInFirstTwoLetters)
+}
+console.log(fuzzyMatch(drivers, "Ne"));
+
+const newDrivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(newDrivers, passedInName) {
+    return newDrivers.filter((e) => e.name === passedInName);
+}
+console.log(matchName(newDrivers, 'Bobby'));
